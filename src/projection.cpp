d7295eed4de1ba67cbd729124a7641a3c667e547
afsoongh <afsoon.gharib@gmail.com>
2023-05-03 13:03:08 -0700

    fixed the image crashing? and VRAM bug

--- src/projection.cpp
+++ src/projection.cpp
@@ -5,14 +5,22 @@
 
 using namespace std;
 float squarePositions[4][2] = {
-    {0.0f, 0.0f},   // bottom-right square
-    {0.1f, 0.1f},   // top-right square
     {-0.1f, 0.1f},  // top-left square
+    {0.1f, 0.1f},   // top-right square
+    {0.0f, 0.0f},   // bottom-right square
     {-0.1f, -0.1f}  // bottom-left square
 };
 int selectedSquare = 0;
 int winWidth = 3840; 
 int winHeight = 2160;
+GLFWwindow* window;
+GLuint fbo;
+GLuint fboTexture;
+GLFWmonitor* monitor = NULL;
+int monitorNumber = 0;
+GLFWmonitor** monitors;
+int monitor_count;
+
 
 ILint texWidth, texHeight;
 
@@ -42,6 +50,11 @@ void saveCoordinates() {
 		int pixelY = (int)((1.0f - squarePositions[i][1]) / 2.0f * winHeight);
 		ROS_ERROR("Square %d position: (%d, %d)\n", i, pixelX, pixelY);
 	}
+//       int pixelWidth = (int)(0.1f * winWidth);
+//       int pixelHeight = (int)(0.1f * winHeight);
+
+	//ROS_ERROR("Pixel dimensions, width is: %d and height is %d", pixelWidth, pixelHeight);
+
 
 }
 
@@ -80,6 +93,52 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
         if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS) {
             saveCoordinates();
         }
+
+		monitors = glfwGetMonitors(&monitor_count);
+        if (glfwGetKey(window, GLFW_KEY_F) == GLFW_RELEASE) {
+            //GLFWmonitor* monitor = glfwGetWindowMonitor(window);
+            //const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+            //// Create a window
+            ////GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "My Window", monitor, NULL);
+
+            //// Make the window fullscreen
+            ////int count;
+            ////GLFWmonitor** monitors = glfwGetMonitors(&count);
+            //glfwSetWindowMonitor(window, NULL, 0, 0, mode->width, mode->height, mode->refreshRate);
+
+
+
+            // find the second monitor (index 1) by checking its position
+            for (int i = 0; i < monitor_count; i++) {
+                const GLFWvidmode* mode = glfwGetVideoMode(monitors[i]);
+                int monitor_x, monitor_y;
+                glfwGetMonitorPos(monitors[i], &monitor_x, &monitor_y);
+                if (monitor_x != 0 || monitor_y != 0) {
+                    monitorNumber = i;
+                    monitor = monitors[i];
+                    break;
+                }
+            }
+
+            // make the window full screen on the second monitor
+            if (monitor) {
+                const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+                glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+            }
+
+        }
+
+        if (glfwGetKey(window, GLFW_KEY_M) == GLFW_RELEASE) {
+            monitorNumber++;
+            monitor = monitors[monitorNumber % monitor_count];
+            if (monitor) {
+                const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+                glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+            }
+
+        }
+
 
 
 
@@ -156,7 +215,7 @@ int main(int argc, char** argv) {
         return -1;
     }
     // Create a window with a 4K resolution (3840x2160)
-    GLFWwindow* window = glfwCreateWindow(winWidth,winHeight, "GLFW 4K Window", NULL, NULL);
+    window = glfwCreateWindow(winWidth,winHeight, "GLFW 4K Window", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -171,7 +230,6 @@ int main(int argc, char** argv) {
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, keyCallback);
     GLuint textureID;
-
     // Set the window resize callback
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -181,18 +239,45 @@ int main(int argc, char** argv) {
     //glTranslatef(0, 0, 0);
 
 
+    // Create an FBO and attach the texture to it
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    glGenTextures(1, &fboTexture);
+    glBindTexture(GL_TEXTURE_2D, fboTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, winWidth, winHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fboTexture, 0);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+
+
+
     while (!glfwWindowShouldClose(window))
     {
-        // Draw squares with updated positions
-        glClear(GL_COLOR_BUFFER_BIT);
-		glGenTextures(1, &textureID);
-		glBindTexture(GL_TEXTURE_2D, textureID);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-		// Load image data into texture
+        //glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+        ////glViewport(0, 0, winWidth, winHeight);
+
+        //// Clear the FBO
+        //glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+  //      // Draw squares with updated positions
+        glClear(GL_COLOR_BUFFER_BIT);
+		//glGenTextures(1, &textureID);
+		//glBindTexture(GL_TEXTURE_2D, textureID);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		//// Load image data into texture
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ilGetInteger(IL_IMAGE_WIDTH),
             ilGetInteger(IL_IMAGE_HEIGHT), 0, GL_RGB,
             GL_UNSIGNED_BYTE, ilGetData());
@@ -205,25 +290,14 @@ int main(int argc, char** argv) {
 			
 
         for (int i = 0; i < 4; i++) {
-   //         //int pixelX = (int)((squarePositions[i][0] + 1.0f) / 2.0f * width);
-   //         //int pixelY = (int)((1.0f - squarePositions[i][1]) / 2.0f * height);
-
-   //         //if (i == selectedSquare) {
-   //         //    glColor3f(1.0f, 1.0f, 1.0f);  // selected square is white
-   //         //}
-   //         //else {
-                //glColor3f(r,g,b);  // other squares are red
-   //         //}
-   //         //drawSquare(pixelX, pixelY, 20.0f);
-   //         float x = squarePositions[i][0];
-   //         float y = squarePositions[i][1];
-   //         float size = 0.1f;
+            glBindTexture(GL_TEXTURE_2D, fboTexture);
             drawRect(squarePositions[i][0], squarePositions[i][1], 0.1f, 0.2f);
         }
 
         // Swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 
         // Exit the loop when the window is closed or escape key is pressed
