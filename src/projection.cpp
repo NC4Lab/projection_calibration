ca96890c4ce51179a33bf8767e569c4231d167c7
afsoongh <afsoon.gharib@gmail.com>
2023-05-11 14:41:59 -0700

    maze is drawn using drawOctagon

--- src/projection.cpp
+++ src/projection.cpp
@@ -9,9 +9,21 @@ const int HEIGHT = 2160;
 
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX = WIDTH / 2.0f;
-float lastY = HEIGHT / 2.0f;
-bool firstMouse = true;
+float mouseOffset = 0.4f;
+float zoomOffset = 1.0f;
+
+
+//Monitor related
+GLFWwindow* window;
+GLFWmonitor* monitor = NULL;
+int monitorNumber = 0;
+GLFWmonitor** monitors;
+int monitor_count;
+
+//camera stuff
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
@@ -23,13 +35,19 @@ const float RADIUS = 0.5f;
 const float HEIGHT_WALL = 0.2f;
 
 // Define the number of octagons in a row and column
-const int ROWS = 7;
-const int COLUMNS = 7;
+const int ROWS = 1;
+const int COLUMNS = 1;
+const int NUMBER_OCT_VERTICES = 8;
+const int NUMBER_WALL_VERTICES = 4;
+const int WALLS = 1;
 
-// Define the vertices for an octagon
-const int NUM_VERTICES_OCTAGON = 8;
-GLfloat vertices_octagon[NUM_VERTICES_OCTAGON * 3];
-const float ANGLE_STEP = 360.0f / NUM_VERTICES_OCTAGON;
+//last one is x,y,z
+GLfloat wall_vertices[ROWS][COLUMNS][WALLS][NUMBER_WALL_VERTICES][3];
+GLfloat octagon_vertices[ROWS][COLUMNS][NUMBER_OCT_VERTICES][3];
+
+
+
+const float ANGLE_STEP = 360.0f / NUMBER_OCT_VERTICES;
 // Define the vertices for the vertical walls of an octagon
 const int NUM_VERTICES_WALL = 4;
 GLfloat vertices_wall[NUM_VERTICES_WALL * 3] = {
@@ -39,6 +57,10 @@ GLfloat vertices_wall[NUM_VERTICES_WALL * 3] = {
     -RADIUS,  RADIUS, HEIGHT_WALL
 };
 
+
+
+
+//make things efficient using indices to reduce duplicate rendering
 // Define the indices for an octagon
 const int NUM_INDICES_OCTAGON = 24;
 GLuint indices_octagon[NUM_INDICES_OCTAGON] = {
@@ -98,6 +120,56 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
+        ROS_ERROR("processing input");
+
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		camera.ProcessMouseMovement(0,mouseOffset);
+    }
+
+
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		camera.ProcessMouseMovement(0,-mouseOffset);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+		camera.ProcessMouseMovement(mouseOffset, 0);
+    }
+
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+		camera.ProcessMouseMovement(-mouseOffset, 0);
+    }
+
+
+    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_RELEASE) {
+		camera.ProcessMouseScroll(mouseOffset);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_RELEASE) {
+		camera.ProcessMouseScroll(-mouseOffset);
+    }
+
+
+    //float xpos = static_cast<float>(xposIn);
+    //float ypos = static_cast<float>(yposIn);
+
+    //if (firstMouse)
+    //{
+    //    lastX = xpos;
+    //    lastY = ypos;
+    //    firstMouse = false;
+    //}
+
+    //float xoffset = xpos - lastX;
+    //float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    //lastX = xpos;
+    //lastY = ypos;
+
+    //camera.ProcessMouseMovement(xoffset, yoffset);
+
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -111,27 +183,28 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 
 // glfw: whenever the mouse moves, this callback is called
+//@rony:bind this to keyboard
 // -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-{
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
-
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
+//void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+//{
+//    float xpos = static_cast<float>(xposIn);
+//    float ypos = static_cast<float>(yposIn);
+//
+//    if (firstMouse)
+//    {
+//        lastX = xpos;
+//        lastY = ypos;
+//        firstMouse = false;
+//    }
+//
+//    float xoffset = xpos - lastX;
+//    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+//
+//    lastX = xpos;
+//    lastY = ypos;
+//
+//    camera.ProcessMouseMovement(xoffset, yoffset);
+//}
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
@@ -140,43 +213,200 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
 
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+    //if (key == GLFW_KEY_1 && action == GLFW_RELEASE) {
+    //    selectedSquare = 0;
+    //}
+    //if (key == GLFW_KEY_2 && action == GLFW_RELEASE) {
+    //    selectedSquare = 1;
+    //}
+    //if (key == GLFW_KEY_3 && action == GLFW_RELEASE) {
+    //    selectedSquare = 2;
+    //}
+    //if (key == GLFW_KEY_4 && action == GLFW_RELEASE) {
+    //    selectedSquare = 3;
+    //}
+
+
+    //// Listen for arrow key input to move selected square
+    //if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE) {
+    //    squarePositions[selectedSquare][0] -= 0.01f;
+    //}
+
+    ////@rony: fix the stuff below
+    //if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE) {
+    //    squarePositions[selectedSquare][0] += 0.01f;
+    //}
+    //if (key == GLFW_KEY_UP && action == GLFW_RELEASE) {
+    //    squarePositions[selectedSquare][1] += 0.01f;
+    //}
+    //if (key == GLFW_KEY_DOWN && action == GLFW_RELEASE) {
+    //    squarePositions[selectedSquare][1] -= 0.01f;
+    //}
+
+    //if (key == GLFW_KEY_ENTER && action == GLFW_RELEASE) {
+    //    saveCoordinates();
+    //}
+
+
+    if (key == GLFW_KEY_F && action == GLFW_RELEASE) {
+        monitors = glfwGetMonitors(&monitor_count);
+        //GLFWmonitor* monitor = glfwGetWindowMonitor(window);
+        //const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+        //// Create a window
+        ////GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "My Window", monitor, NULL);
+
+        //// Make the window fullscreen
+        ////int count;
+        ////GLFWmonitor** monitors = glfwGetMonitors(&count);
+        //glfwSetWindowMonitor(window, NULL, 0, 0, mode->width, mode->height, mode->refreshRate);
+
+
+
+        // find the second monitor (index 1) by checking its position
+        for (int i = 0; i < monitor_count; i++) {
+            const GLFWvidmode* mode = glfwGetVideoMode(monitors[i]);
+            int monitor_x, monitor_y;
+            glfwGetMonitorPos(monitors[i], &monitor_x, &monitor_y);
+            if (monitor_x != 0 || monitor_y != 0) {
+                monitorNumber = i;
+                monitor = monitors[i];
+                break;
+            }
+        }
+
+        // make the window full screen on the second monitor
+        if (monitor) {
+            const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+            glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+        }
+
+    }
+
+    if (key == GLFW_KEY_M && action == GLFW_RELEASE) {
+        monitors = glfwGetMonitors(&monitor_count);
+        monitorNumber++;
+        monitor = monitors[monitorNumber % monitor_count];
+        if (monitor) {
+            const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+            glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+        }
+
+    }
+}
+
+void drawWall(int i,int j,int k) {
+
+    glBegin(GL_QUADS);
+
+    //glTexCoord2f(x, y);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3fv(wall_vertices[i][j][k][0]);
+
+    //glTexCoord2f(x+texWidth, y);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3fv(wall_vertices[i][j][k][1]);
+
+    //glTexCoord2f(x+texWidth, y+texHeight);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3fv(wall_vertices[i][j][k][2]);
+
+    //glTexCoord2f(x, y+texHeight);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3fv(wall_vertices[i][j][k][3]);
+    glEnd();
+
+}
+
+void drawOctagon(float x, float y, float z, float size, float height)
+{
+    glBegin(GL_QUADS);
+    // draw the bottom face
+    //for (int i = 0; i < 8; i++)
+    //{
+    //    float angle = i * 2 * M_PI / 8;
+    //    glVertex3f(x + size * cos(angle), y + size * sin(angle), z);
+    //}
+    // draw the walls
+    for (int i = 0; i < 8; i++)
+    {
+        float angle1 = (i + 0.5) * 2 * M_PI / 8;
+        float angle2 = (i + 1.5) * 2 * M_PI / 8;
+        glVertex3f(x + size * cos(angle1), y + size * sin(angle1), z);
+        glVertex3f(x + size * cos(angle2), y + size * sin(angle2), z);
+        glVertex3f(x + size * cos(angle2), y + size * sin(angle2), z + height);
+        glVertex3f(x + size * cos(angle1), y + size * sin(angle1), z + height);
+    }
+    // draw the top face
+    //for (int i = 0; i < 8; i++)
+    //{
+    //    float angle = i * 2 * M_PI / 8;
+    //    glVertex3f(x + size * cos(angle), y + size * sin(angle), z + height);
+    //}
+    glEnd();
+}
 
 int main(int argc, char** argv)
 {
     const float offset_angle = 22.5f;
-    for (int i = 0; i < NUM_VERTICES_OCTAGON; ++i) {
-        float angle = ANGLE_STEP * i;
-        vertices_octagon[i * 3] = RADIUS * std::cos(angle * M_PI / 180.0f);
-        vertices_octagon[i * 3 + 1] = RADIUS * std::sin(angle * M_PI / 180.0f);
-        vertices_octagon[i * 3 + 2] = 0.0f;
-    }
-    //weird formation.
+    // for future stuff
+   // for (int i = 0; i < ROWS; i++) {
+
+   //     for (int j = 0; j < COLUMNS; j++) {
+
+			//for (int k = 0; k < WALLS; k++) {
+
+			//	for (int l = 0; l < NUMBER_WALL_VERTICES; l++) {
+   //                    
+   //                  for (int m = 0; m < 3) {
+
+   //                     /* wall_vertices[i][j][k][l][m]*/
+			//			vertices_octagon[index + k * 3] = RADIUS * std::cos(angle * M_PI / 180.0) + x_offset;
+			//			vertices_octagon[index + k * 3 + 1] = RADIUS * std::sin(angle * M_PI / 180.0f) + y_offset;
+			//			vertices_octagon[index + k * 3 + 2] = HEIGHT_WALL;
+
+   //                  }
+			//	}
+			//}
+   //     }
+   // }
+
+
+ //   wall_vertices[0][0][0][0][0] = 0.0f;
+ //   wall_vertices[0][0][0][0][1] = 0.0f;
+ //   wall_vertices[0][0][0][0][2] = 0.0f;
+
+ //   
+ //   wall_vertices[0][0][0][1][0] = 0.1f;
+	//wall_vertices[0][0][0][1][1] = 0.0f;
+	//wall_vertices[0][0][0][1][2] = 0.0f;
+
+ //   wall_vertices[0][0][0][2][0] = 0.1f;
+	//wall_vertices[0][0][0][2][1] = 0.1f;
+	//wall_vertices[0][0][0][2][2] = 0.0f;
+
+ //   wall_vertices[0][0][0][3][0] = 0.0f;
+	//wall_vertices[0][0][0][3][1] = 0.1f;
+	//wall_vertices[0][0][0][3][2] = 0.0f;
+
+
+
+    ////attempt for 7x7 formation, 2.5 is the offset between octagons
     //for (int i = 0; i < ROWS; ++i) {
     //    for (int j = 0; j < COLUMNS; ++j) {
-    //        int index = (i * COLUMNS + j) * NUM_VERTICES_OCTAGON * 3;
+    //        int index = ((i * COLUMNS) + j) * NUM_VERTICES_OCTAGON * 3;
+    //        float x_offset = (j - 0.3f) * 0.25f * RADIUS;
+    //        float y_offset = (i - 0.3f) * 0.25f * RADIUS;
     //        for (int k = 0; k < NUM_VERTICES_OCTAGON; ++k) {
     //            float angle = ANGLE_STEP * k;
-    //            vertices_octagon[index + k * 3] = RADIUS * std::cos(angle * M_PI / 180.0) * std::cos((k * 45.0 + offset_angle) * M_PI / 180.0);
-    //            vertices_octagon[index + k * 3 + 1] = RADIUS * std::sin(angle * M_PI / 180.0f);
-    //            vertices_octagon[index + k * 3 + 2] = HEIGHT_WALL * (i + j);
+    //            vertices_octagon[index + k * 3] = RADIUS * std::cos(angle * M_PI / 180.0) + x_offset;
+    //            vertices_octagon[index + k * 3 + 1] = RADIUS * std::sin(angle * M_PI / 180.0f) + y_offset;
+    //            vertices_octagon[index + k * 3 + 2] = HEIGHT_WALL;
     //        }
     //    }
     //}
-
-    //attempt for 7x7 formation, 2.5 is the offset between octagons
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLUMNS; ++j) {
-            int index = ((i * COLUMNS) + j) * NUM_VERTICES_OCTAGON * 3;
-            float x_offset = 0;//(j - 3.0f) * 2.5f * RADIUS;
-            float y_offset = 0;// (i - 3.0f) * 2.5f * RADIUS;
-            for (int k = 0; k < NUM_VERTICES_OCTAGON; ++k) {
-                float angle = ANGLE_STEP * k;
-                vertices_octagon[index + k * 3] = RADIUS * std::cos(angle * M_PI / 180.0) + x_offset;
-                vertices_octagon[index + k * 3 + 1] = RADIUS * std::sin(angle * M_PI / 180.0f) + y_offset;
-                vertices_octagon[index + k * 3 + 2] = HEIGHT_WALL;
-            }
-        }
-    }
 
 
     ros::init(argc, argv, "Projection", ros::init_options::AnonymousName);
@@ -216,7 +446,7 @@ int main(int argc, char** argv)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "GLFW 4K Window", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "GLFW 4K Window", NULL, NULL);
 
     // Set the window as the current OpenGL context
     glfwMakeContextCurrent(window);
@@ -224,7 +454,7 @@ int main(int argc, char** argv)
 
     gladLoadGL();
     glfwSwapInterval(1);
-    //glfwSetKeyCallback(window, keyCallback);
+    glfwSetKeyCallback(window, keyCallback);
     // Make the window's context current
 
     glEnable(GL_DEPTH_TEST);
@@ -235,34 +465,35 @@ int main(int argc, char** argv)
     unsigned int texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ilGetInteger(IL_IMAGE_WIDTH),
+            ilGetInteger(IL_IMAGE_HEIGHT), 0, GL_RGB,
+            GL_UNSIGNED_BYTE, ilGetData());
     glGenerateMipmap(GL_TEXTURE_2D);
 
 
 
-
     // Set up the vertex buffer objects (VBOs) and element buffer objects (EBOs)
-    GLuint VBO_octagon, VBO_wall, VAO_octagon, VAO_wall, EBO_octagon;
+    GLuint VBO_wall, VAO_wall;  //, EBO_octagon, VBO_octagon, VAO_octagon;
 
-    glGenBuffers(1, &VBO_octagon);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_octagon);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_octagon), vertices_octagon, GL_STATIC_DRAW);
+    //glGenBuffers(1, &VBO_octagon);
+    //glBindBuffer(GL_ARRAY_BUFFER, VBO_octagon);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_octagon), vertices_octagon, GL_STATIC_DRAW);
 
     glGenBuffers(1, &VBO_wall);
     glBindBuffer(GL_ARRAY_BUFFER, VBO_wall);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_wall), vertices_wall, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(wall_vertices), wall_vertices, GL_STATIC_DRAW);
 
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
+    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    //glEnableVertexAttribArray(1);
 
-    glGenVertexArrays(1, &VAO_octagon);
-    glBindVertexArray(VAO_octagon);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_octagon);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
-    glEnableVertexAttribArray(0);
+    //glGenVertexArrays(1, &VAO_octagon);
+    //glBindVertexArray(VAO_octagon);
+    //glBindBuffer(GL_ARRAY_BUFFER, VBO_octagon);
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    //glEnableVertexAttribArray(0);
 
     glGenVertexArrays(1, &VAO_wall);
     glBindVertexArray(VAO_wall);
@@ -270,9 +501,9 @@ int main(int argc, char** argv)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glGenBuffers(1, &EBO_octagon);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_octagon);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_octagon), indices_octagon, GL_STATIC_DRAW);
+    //glGenBuffers(1, &EBO_octagon);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_octagon);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_octagon), indices_octagon, GL_STATIC_DRAW);
 
     //// Set up the shader program
     //GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -331,10 +562,10 @@ int main(int argc, char** argv)
         ourShader.setMat4("projection", projection);
 
         // camera/view transformation
-        glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMat4("view", view);
+        //glm::mat4 view = camera.GetViewMatrix();
+        //ourShader.setMat4("view", view);
 
-		glm:mat4 model = glm::mat4(1.0f); 
+		glm::mat4 model = glm::mat4(1.0f); 
         ourShader.setMat4("model", model);
 
         //ourShader.setFloat("colour", wallColor);
@@ -342,14 +573,14 @@ int main(int argc, char** argv)
 
         //    // Set the uniform variables for the shader program
         //glUseProgram(shaderProgram);
- /*       glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+ /*     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 */
         //    // Draw the octagons
-        glBindVertexArray(VAO_octagon);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_octagon);
-        glDrawElements(GL_TRIANGLES, sizeof(indices_octagon), GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(VAO_octagon);
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_octagon);
+        //glDrawElements(GL_TRIANGLES, sizeof(indices_octagon), GL_UNSIGNED_INT, 0);
 
 
         //bind wall image texture, not sure if it's placed correctly here
@@ -365,10 +596,27 @@ int main(int argc, char** argv)
 
         ourShader.use();
         ourShader.setInt("texture1", 0); // Set the texture uniform to 0 (GL_TEXTURE0)
+        glm::mat4 view = camera.GetViewMatrix(); //glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        //ourShader.setMat4("view", view);
+
+        float angle = 20.0f;
+        view = glm::rotate(view, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        // pass the view matrix to the shader
+        ourShader.setMat4("view", view);
 
 
 
-        glDrawArrays(GL_LINES, 0, sizeof(vertices_wall) / sizeof(vertices_wall[0]));
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //drawWall(0, 0, 0);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++)
+            {
+               
+			drawOctagon((float) i, (float) j, 1, 0.5, 0.5);
+            }
+        }
 
         //    // Swap the front and back buffers
         glfwSwapBuffers(window);
@@ -378,11 +626,11 @@ int main(int argc, char** argv)
     }
 
     ////// Clean up
-    glDeleteBuffers(1, &VBO_octagon);
+    //glDeleteBuffers(1, &VBO_octagon);
     glDeleteBuffers(1, &VBO_wall);
-    glDeleteVertexArrays(1, &VAO_octagon);
+    //glDeleteVertexArrays(1, &VAO_octagon);
     glDeleteVertexArrays(1, &VAO_wall);
-    glDeleteBuffers(1, &EBO_octagon);
+    //glDeleteBuffers(1, &EBO_octagon);
     //glDeleteProgram(shaderProgram);
 
     ////// Terminate GLFW
